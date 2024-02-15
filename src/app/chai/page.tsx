b66c6a724229b91fcfa1a20fedc6c812c5c725db
Sujal <sujalkesharwani220@gmail.com>
2024-02-15 22:20:14 +0530

    ShadCN started

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,10 @@
+export default function ChaiPage() {
+  return (
+    <>
+      <main className="h-full flex justify-center flex-col items-center">
+        <div className="uppercase">Chai page happy</div>
+        <button className="bg-blue-500 px-6 py-2 rounded hover:bg-blue-800 font-semibold cursor-pointer mt-3">ShadCN</button>
+      </main>
+    </>
+  );
+}
